81c051c55c265e804ffdd3b64cfe6f689d87e2f2
Areen04 <161502994+Areen04@users.noreply.github.com>
2025-03-25 23:29:41 +0300

    re

--- search_suggestions.php
+++ search_suggestions.php
@@ -6,9 +6,12 @@ header('Content-Type: application/json');
 $query = isset($_GET['query']) ? $_GET['query'] : '';
 $search = "%" . $query . "%";
 
-$sql = "SELECT ISBN, title FROM book WHERE title LIKE ? LIMIT 10";
+
+$sql = "SELECT ISBN, title FROM book 
+        WHERE title LIKE ? OR Author LIKE ? OR Genre LIKE ?
+        LIMIT 10";
 $stmt = $connection->prepare($sql);
-$stmt->bind_param("s", $search);
+$stmt->bind_param("sss", $search, $search, $search);
 $stmt->execute();
 $result = $stmt->get_result();
 
